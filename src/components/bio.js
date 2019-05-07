fd79f94e1b00194f1647a55c35e323501459874a
Mayuresh Narendra Waykole <manarend@cisco.com>
2019-05-06 17:34:52 -0700

    last of assets remove

--- src/components/bio.js
+++ src/components/bio.js
@@ -24,19 +24,6 @@ function Bio() {
               marginBottom: rhythm(2.5),
             }}
           >
-            {/* <Image
-              fixed={data.avatar.childImageSharp.fixed}
-              alt={author}
-              style={{
-                marginRight: rhythm(1 / 2),
-                marginBottom: 0,
-                minWidth: 50,
-                borderRadius: `100%`,
-              }}
-              imgStyle={{
-                borderRadius: `50%`,
-              }}
-            /> */}
             <p>
               Written by <strong>{author}</strong>.
               {` `}
@@ -53,13 +40,6 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-      childImageSharp {
-        fixed(width: 50, height: 50) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
     site {
       siteMetadata {
         author
