c5c27411cde728e2c8a99e3a21f2dfdb0126d3c2
Mayuresh Narendra Waykole <manarend@cisco.com>
2019-05-06 17:51:49 -0700

    resolves #3

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,30 +15,21 @@ class BlogIndex extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
-          title="All posts"
+          title="Tech Blog"
           keywords={[`blog`, `mayuresh`, `waykole`, `programming`]}
         />
         <Bio />
         {posts.map(({ node }) => {
-          console.log(node)
           const title = node.frontmatter.title || node.fields.slug
           return (
             <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+              <Link style={{ boxShadow: `none`, color: `black` }} to={node.fields.slug}>
+                <h3 style={{ marginBottom: rhythm(1 / 4) }}>
                   {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.excerpt,
-                }}
-              />
+                </h3>
+                <small>{node.frontmatter.date}</small>
+                <p dangerouslySetInnerHTML={{ __html: node.excerpt, }} />
+              </Link>
             </div>
           )
         })}
